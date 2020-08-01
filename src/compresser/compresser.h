43b85e5774b6a670af9c49e77645994cb1e1967c
João Gabriel Trombeta <joaog.trombeta@gmail.com>
2020-08-01 15:41:20 -0300

    Use zlib without boost

--- src/compresser/compresser.h
+++ src/compresser/compresser.h
@@ -1,12 +1,27 @@
-#ifndef __GZIP_H__
-#define __GZIP_H__
+// Copyright 2007 Timo Bingmann <tb@panthema.net>
+// Distributed under the Boost Software License, Version 1.0.
+// (See http://www.boost.org/LICENSE_1_0.txt)
+//
+// Original link http://panthema.net/2007/0328-ZLibString.html
 
+#ifndef COMPRESSER_H
+#define COMPRESSER_H
+
+#include <string>
+#include <string.h>
+#include <stdexcept>
+#include <iostream>
+#include <iomanip>
 #include <sstream>
-#include <boost/iostreams/filtering_streambuf.hpp>
-#include <boost/iostreams/copy.hpp>
-#include <boost/iostreams/filter/gzip.hpp>
 
-std::string compress(const std::string& data);
-std::string decompress(const std::string& data);
+#include <zlib.h>
 
-#endif // __GZIP_H__
+/** Compress a STL string using zlib with given compression level and return
+  * the binary data. */
+std::string compress(const std::string& str,
+                            int compressionlevel = Z_BEST_COMPRESSION);
+
+/** Decompress an STL string using zlib and return the original data. */
+std::string decompress(const std::string& str);
+
+#endif
