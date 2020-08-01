5bdedd04c6c8aaaa8fd87baf84a6ee4bcede9873
João Gabriel Trombeta <joaog.trombeta@gmail.com>
2020-08-01 15:41:20 -0300

    Use zlib without boost

--- src/compresser/compresser.cpp
+++ src/compresser/compresser.cpp
@@ -1,32 +1,93 @@
+// Copyright 2007 Timo Bingmann <tb@panthema.net>
+// Distributed under the Boost Software License, Version 1.0.
+// (See http://www.boost.org/LICENSE_1_0.txt)
+//
+// Original link http://panthema.net/2007/0328-ZLibString.html
+
 #include "compresser.h"
 
 
-std::string compress(const std::string& data)
+/** Compress a STL string using zlib with given compression level and return
+  * the binary data. */
+std::string compress(const std::string& str,
+                            int compressionlevel/* = Z_BEST_COMPRESSION*/)
 {
-	namespace bio = boost::iostreams;
+    z_stream zs;                        // z_stream is zlib's control structure
+    memset(&zs, 0, sizeof(zs));
 
-	std::stringstream compressed;
-	std::stringstream origin(data);
+    if (deflateInit(&zs, compressionlevel) != Z_OK)
+        throw(std::runtime_error("deflateInit failed while compressing."));
 
-	bio::filtering_streambuf<bio::input> out;
-	out.push(bio::gzip_compressor(bio::gzip_params(bio::gzip::best_compression)));
-	out.push(origin);
-	bio::copy(out, compressed);
+    zs.next_in = (Bytef*)str.data();
+    zs.avail_in = str.size();           // set the z_stream's input
 
-	return compressed.str();
+    int ret;
+    char outbuffer[32768];
+    std::string outstring;
+
+    // retrieve the compressed bytes blockwise
+    do {
+        zs.next_out = reinterpret_cast<Bytef*>(outbuffer);
+        zs.avail_out = sizeof(outbuffer);
+
+        ret = deflate(&zs, Z_FINISH);
+
+        if (outstring.size() < zs.total_out) {
+            // append the block to the output string
+            outstring.append(outbuffer,
+                             zs.total_out - outstring.size());
+        }
+    } while (ret == Z_OK);
+
+    deflateEnd(&zs);
+
+    if (ret != Z_STREAM_END) {          // an error occurred that was not EOF
+        std::ostringstream oss;
+        oss << "Exception during zlib compression: (" << ret << ") " << zs.msg;
+        throw(std::runtime_error(oss.str()));
+    }
+
+    return outstring;
 }
 
-std::string decompress(const std::string& data)
+/** Decompress an STL string using zlib and return the original data. */
+std::string decompress(const std::string& str)
 {
-	namespace bio = boost::iostreams;
+    z_stream zs;                        // z_stream is zlib's control structure
+    memset(&zs, 0, sizeof(zs));
 
-	std::stringstream compressed(data);
-	std::stringstream decompressed;
+    if (inflateInit(&zs) != Z_OK)
+        throw(std::runtime_error("inflateInit failed while decompressing."));
 
-	bio::filtering_streambuf<bio::input> out;
-	out.push(bio::gzip_decompressor());
-	out.push(compressed);
-	bio::copy(out, decompressed);
+    zs.next_in = (Bytef*)str.data();
+    zs.avail_in = str.size();
 
-	return decompressed.str();
+    int ret;
+    char outbuffer[32768];
+    std::string outstring;
+
+    // get the decompressed bytes blockwise using repeated calls to inflate
+    do {
+        zs.next_out = reinterpret_cast<Bytef*>(outbuffer);
+        zs.avail_out = sizeof(outbuffer);
+
+        ret = inflate(&zs, 0);
+
+        if (outstring.size() < zs.total_out) {
+            outstring.append(outbuffer,
+                             zs.total_out - outstring.size());
+        }
+
+    } while (ret == Z_OK);
+
+    inflateEnd(&zs);
+
+    if (ret != Z_STREAM_END) {          // an error occurred that was not EOF
+        std::ostringstream oss;
+        oss << "Exception during zlib decompression: (" << ret << ") "
+            << zs.msg;
+        throw(std::runtime_error(oss.str()));
+    }
+
+    return outstring;
 }
