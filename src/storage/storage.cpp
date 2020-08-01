3551f2e53dd40cd4d8fdcd1ecf2e8f5a58f108eb
João Gabriel Trombeta <joaog.trombeta@gmail.com>
2020-08-01 15:02:01 -0300

    Compress data before store and retrieve it

--- src/storage/storage.cpp
+++ src/storage/storage.cpp
@@ -8,18 +8,18 @@ std::string template_value(VALUE_SIZE, '*');
 
 
 std::string Storage::read(int key) const {
-    return storage_.at(key);
+    return decompress(storage_.at(key));
 }
 
 void Storage::write(int key, const std::string& value) {
-    storage_[key] = template_value;
+    storage_[key] = compress(template_value);
 }
 
 std::vector<std::string> Storage::scan(int start, int length) {
     auto values = std::vector<std::string>();
     for (auto i = 0; i < length; i++) {
         auto key = (start + i) % storage_.size();
-        values.push_back(storage_[key]);
+        values.push_back(decompress(storage_[key]));
     }
     return values;
 }
