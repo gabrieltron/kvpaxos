3a3ee4dde578632357cb74641bbfb672d256dde6
João Gabriel Trombeta <joaog.trombeta@gmail.com>
2020-08-13 22:00:11 -0300

    Ignore exception from decompression

--- src/storage/storage.cpp
+++ src/storage/storage.cpp
@@ -8,7 +8,15 @@ std::string template_value(VALUE_SIZE, '*');
 
 
 std::string Storage::read(int key) const {
-    return decompress(storage_.at(key));
+    try {
+        return decompress(storage_.at(key));
+    } catch(...) {
+        // I'm not sure why sometimes decompression fails.
+        // It fails in what seems to be random keys and in less
+        // than 0.00001% of calls, so lets just ignore it for now.
+        // It'll be wise to investigate
+        return template_value;
+    }
 }
 
 void Storage::write(int key, const std::string& value) {
@@ -19,7 +27,7 @@ std::vector<std::string> Storage::scan(int start, int length) {
     auto values = std::vector<std::string>();
     for (auto i = 0; i < length; i++) {
         auto key = (start + i) % storage_.size();
-        values.push_back(decompress(storage_[key]));
+        values.push_back(read(key));
     }
     return values;
 }
